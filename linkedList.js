821226c581e2e0569fe096f65ad12c57074925b7
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-17 17:55:43 -1000

    adds console logging to add to verify it works

--- linkedList.js
+++ linkedList.js
@@ -23,19 +23,26 @@ function linkedListGenerator() {
     let newNode = {};
     newNode.value = value;
     newNode.next = null;
+    let current = head;
     if (head === null) {
       head = newNode;
-    }
-    if (head === tail) {
-      head.next = newNode;
+    } else {
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
     tail = newNode;
+    // console.log("THE NEW VALUE IS: " + value);
+    // console.log(newNode);
+    // console.log(head);
     return newNode;
   }
 
   // 1) Find the node you are searching for and keep track of previous node
   // 2) When found, update previous node's next to point to the next node referenced by node to be deleted
   // 3) Delete found node from memory
+
   function remove() {
     return;
   }
@@ -44,32 +51,30 @@ function linkedListGenerator() {
   // 2) Check if value matches what you're searching for, if found return that node
   // 3) If not found, move to the next node via current node's .next property
   // 4) Repeat until .next is null (tail / end of list)
+
+  let current = head;
+
   function get(number) {
-    // console.log(head);
-    // console.log(head.next);
-    // console.log(getHead());
-    // console.log(getTail());
-    // if (number < 0) {
-    //   return;
-    // }
-    // if (number === 0) {
-    //   return head;
-    // }
-    // if (number > 0) {
-    //   console.log(number);
-    //   console.log(head);
-    //   head = head.next;
-    //   console.log(head);
-    //   get(--number);
-    //   console.log(number);
-    //   return head;
-    // }
-    // return head;
+    if (number < 0) {
+      return;
+    }
+    if (number === 0) {
+      console.log("THE NODE AT POSITION " + number + " IS: ");
+      console.log(current);
+      return current;
+    }
+    if (number > 0) {
+      current = current.next;
+      get(--number);
+      // console.log(current);
+      return current;
+    }
   }
 
   // 1) Find the position/node in your linked list where you want to insert your new node after
   // 2) Update the next property of the new node to point to the node that the target node currently points to
   // 3) Update the next property of the node you want to insert after to point to the new node
+
   function insert() {
     return;
   }
